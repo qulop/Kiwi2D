4e9f6f53a6bf60977194cf40d50053237c5332b7
qulop <qlopsimail@gmail.com>
2026-05-30 21:33:37 +0300

    feat(common): added partial specialization for `wchar_t` for the `BasicCString<T>`

--- engine/common/types/CString.hpp
+++ engine/common/types/CString.hpp
@@ -5,22 +5,29 @@
 
 #include <memory/Memory.hpp>
 
+#include <cstring>
+#include <cwchar>
+
 
 namespace Kiwi {
-	template<typename T>
-	struct BasicCString final {
+    template<typename T>
+    struct BasicCString;
+
+
+	template<>
+	struct BasicCString<char> final {
 	public:
-		using CharType = T;
+		using CharType = char;
 		
 	public:
 		BasicCString() = delete;
 
 	public:
-        KIWI_FORCEINLINE static CharType* StrCpy(CharType* dst, size_t dstsz, const CharType* src) {
+        static CharType* StrCpy(CharType* dst, size_t dstsz, const CharType* src) {
             return (strcpy_s(dst, dstsz, src) == 0) ? dst : nullptr;
 		}
 
-        KIWI_FORCEINLINE static CharType* StrCpyN(CharType* dst, size_t dstsz, CharType* src, size_t count) {
+        static CharType* StrCpyN(CharType* dst, size_t dstsz, const CharType* src, size_t count) {
             return (strncpy_s(dst, dstsz, src, count) == 0) ? dst : nullptr;
         }
 
@@ -35,13 +42,13 @@ namespace Kiwi {
                 return nullptr;
             }
 
-            for (auto i = 0; i < numToCopy; i++) {
+            for (size_t i = 0; i < numToCopy; i++) {
                 size_t srcIndex = i + offset;
                 size_t srcLen = StrLen(src[srcIndex]);
 
                 dstArr[i] = KIWI_NOTHROW_NEW CharType[srcLen + 1];
                 if (!dstArr[i]) {
-                    for (auto j = 0; j < i; j++) {
+                    for (size_t j = 0; j < i; j++) {
                         delete[] dstArr[j];
                     }
 
@@ -56,80 +63,156 @@ namespace Kiwi {
             return dstArr;
         }
 
-        KIWI_FORCEINLINE static CharType* StrCat(CharType* dst, size_t dstsz, CharType* src) {
+        static CharType* StrCat(CharType* dst, size_t dstsz, const CharType* src) {
             return (strcat_s(dst, dstsz, src) == 0) ? dst : nullptr;
         }
 
-        KIWI_FORCEINLINE static CharType* StrCatN(CharType* dst, size_t dstsz, const CharType* src, size_t count) {
-            return (strncat_s(dst, dstsz, src, count)) ? dst : nullptr;
+        static CharType* StrCatN(CharType* dst, size_t dstsz, const CharType* src, size_t count) {
+            return (strncat_s(dst, dstsz, src, count) == 0) ? dst : nullptr;
         }
 
         template<typename TReturnType = size_t>
-        KIWI_FORCEINLINE static TReturnType StrLen(const CharType* str) {
-            return BasicCast::To<TReturnType>(std::strlen(str));
+        static TReturnType StrLen(const CharType* str) {
+            return static_cast<TReturnType>(std::strlen(str));
         }
 
-        KIWI_FORCEINLINE static i32 StrCmp(const CharType* lhs, const CharType* rhs) {
-            if constexpr (std::same_as<CharType, char>) {
-                return std::strcmp(lhs, rhs);
-            }
-            else if constexpr (std::same_as<CharType, wchar_t>) {
-                return std::wcscmp(lhs, rhs);
-            }
+        static i32 StrCmp(const CharType* lhs, const CharType* rhs) {
+            return std::strcmp(lhs, rhs);
         }
 
-        KIWI_FORCEINLINE static bool StrCmpBool(const CharType* lhs, const CharType* rhs) {
+        static bool StrCmpBool(const CharType* lhs, const CharType* rhs) {
             return (StrCmp(lhs, rhs) == 0);
         }
 
-        KIWI_NODISCARD KIWI_FORCEINLINE static CharType* New(size_t sz) {
-            return (CharType*)Memory::Malloc(sz);
+        KIWI_NODISCARD static CharType* New(size_t sz) {
+            return static_cast<CharType*>(Memory::Malloc(sz * sizeof(CharType)));
         }
 
-        KIWI_FORCEINLINE static void Delete(CharType* ptr) {
+        static void Delete(CharType* ptr) {
             Memory::Free(ptr);
         }
 	};
+
+    template<>
+    struct BasicCString<wchar_t> final {
+    public:
+        using CharType = wchar_t;
+
+    public:
+        BasicCString() = delete;
+
+    public:
+        static CharType* StrCpy(CharType* dst, size_t dstsz, const CharType* src) {
+            return (wcscpy_s(dst, dstsz, src) == 0) ? dst : nullptr;
+        }
+
+        static CharType* StrCpyN(CharType* dst, size_t dstsz, const CharType* src, size_t count) {
+            return (wcsncpy_s(dst, dstsz, src, count) == 0) ? dst : nullptr;
+        }
+
+        static CharType** StrArrCpy(const CharType** src, size_t len, size_t offset = 0) {
+            if (!src || offset > len) {
+                return nullptr;
+            }
+
+            size_t numToCopy = len - offset;
+            auto** dstArr = KIWI_NOTHROW_NEW CharType*[numToCopy + 1];
+            if (!dstArr) {
+                return nullptr;
+            }
+
+            for (size_t i = 0; i < numToCopy; i++) {
+                size_t srcIndex = i + offset;
+                size_t srcLen = StrLen(src[srcIndex]);
+
+                dstArr[i] = KIWI_NOTHROW_NEW CharType[srcLen + 1];
+                if (!dstArr[i]) {
+                    for (size_t j = 0; j < i; j++) {
+                        delete[] dstArr[j];
+                    }
+
+                    delete[] dstArr;
+                    return nullptr;
+                }
+
+                StrCpy(dstArr[i], srcLen + 1, src[srcIndex]);
+            }
+
+            dstArr[numToCopy] = nullptr;
+            return dstArr;
+        }
+
+        static CharType* StrCat(CharType* dst, size_t dstsz, const CharType* src) {
+            return (wcscat_s(dst, dstsz, src) == 0) ? dst : nullptr;
+        }
+
+        static CharType* StrCatN(CharType* dst, size_t dstsz, const CharType* src, size_t count) {
+            return (wcsncat_s(dst, dstsz, src, count) == 0) ? dst : nullptr;
+        }
+
+        template<typename TReturnType = size_t>
+        static TReturnType StrLen(const CharType* str) {
+            return static_cast<TReturnType>(std::wcslen(str));
+        }
+
+        static i32 StrCmp(const CharType* lhs, const CharType* rhs) {
+            return std::wcscmp(lhs, rhs);
+        }
+
+        static bool StrCmpBool(const CharType* lhs, const CharType* rhs) {
+            return (StrCmp(lhs, rhs) == 0);
+        }
+
+        KIWI_NODISCARD static CharType* New(size_t sz) {
+            return static_cast<CharType*>(Memory::Malloc(sz * sizeof(CharType)));
+        }
+
+        static void Delete(CharType* ptr) {
+            Memory::Free(ptr);
+        }
+    };
+
 
 	class CMemoryTools {
 	public:
 		using RawMemoryPtr = void*;
 
 	public:
-        KIWI_FORCEINLINE static RawMemoryPtr MemSet(RawMemoryPtr dst, i32 ch, size_t count) {
+        static RawMemoryPtr MemSet(RawMemoryPtr dst, i32 ch, size_t count) {
 			return std::memset(dst, ch, count);
 		}
 
-        KIWI_FORCEINLINE static RawMemoryPtr MemCpy(RawMemoryPtr dst, const RawMemoryPtr src, size_t count) {
+        static RawMemoryPtr MemCpy(RawMemoryPtr dst, const RawMemoryPtr src, size_t count) {
 			return std::memcpy(dst, src, count);
 		}
 
-        KIWI_FORCEINLINE static i32 MemCmp(const RawMemoryPtr lhs, const RawMemoryPtr rhs, size_t count) {
+        static i32 MemCmp(const RawMemoryPtr lhs, const RawMemoryPtr rhs, size_t count) {
 			return std::memcmp(lhs, rhs, count);
 		}
 
-        KIWI_FORCEINLINE static bool MemCmpBool(const RawMemoryPtr lhs, const RawMemoryPtr rhs, size_t count) {
+        static bool MemCmpBool(const RawMemoryPtr lhs, const RawMemoryPtr rhs, size_t count) {
             return (MemCmp(lhs, rhs, count) == 0);
         }
 
-        KIWI_FORCEINLINE static void MemSetSafe(RawMemoryPtr mem, size_t count) {
+        static void MemSetSafe(RawMemoryPtr mem, size_t count) {
             // TODO: Not as safe, as it could be
-            volatile byte* ptr = BasicCast::To<byte*>(mem);
+            volatile byte* ptr = static_cast<byte*>(mem);
             for (size_t i = 0; i < count; i++) {
                 ptr[i] = 0;
             }
         }
 
 	    template<typename T>
-	    KIWI_FORCEINLINE static T* Malloc(size_t sz) {
+	    static T* Malloc(size_t sz) {
             return CastTo<T*>(Memory::Malloc(sz));
         }
 
-	    KIWI_FORCEINLINE static void Free(void* ptr) {
+	    static void Free(void* ptr) {
             Memory::Free(ptr);
         }
     };
 
-	using CString = BasicCString<char>;
+
+    using CString = BasicCString<char>;
     using CWideString = BasicCString<wchar_t>;
 }
