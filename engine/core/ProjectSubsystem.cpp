d06f7d724604275395a183dd77a5e93e4706a3a5
qulop <qlopsimail@gmail.com>
2026-07-02 21:41:05 +0300

    feat(core): now `ProjectSubsystem` will load an active project only if corresponding command-line option was provided. Besides this, during the initialization it will also validate all other existing projects

--- engine/core/ProjectSubsystem.cpp
+++ engine/core/ProjectSubsystem.cpp
@@ -1,13 +1,24 @@
 #include "ProjectSubsystem.hpp"
 
+#include <complex>
 #include <core/Project.hpp>
+
+#include <common/database/SQLiteDatabase.hpp>
+#include <common/filesystem/File.hpp>
 
 #include <platform/Platform.hpp>
 
+#include <nlohmann/json.hpp>
+
 
 namespace Kiwi {
-    ProjectSubsystem::ProjectSubsystem(std::filesystem::path projectPath) :
-        m_activeProjectPath(std::move(projectPath))
+    std::filesystem::path ProjectSubsystem::GetProjectsDatabasePath() {
+        return Platform::GetExecutableDirectoryPath() / PROJECTS_DB_FILE_NAME;
+    }
+
+
+    ProjectSubsystem::ProjectSubsystem(Opt<i32> activeProjectID) :
+        m_activeProjectID(std::move(activeProjectID))
     {}
 
     bool ProjectSubsystem::Init() {
@@ -15,26 +26,116 @@ namespace Kiwi {
             return false;
         }
 
-        if (m_activeProjectPath.empty()) {
-            m_activeProjectPath = Platform::GetApplicationPath().parent_path();
+        if (!LoadProjectsDatabase()) {
+            return false;
         }
 
-        if (Result<std::shared_ptr<Project>> project = Project::Open(m_activeProjectPath)) {
-            m_activeProject = *project;
-            return true;
+
+        if (m_activeProjectID.HasValue()) {
+            if (!LoadActiveProject()) {
+                return false;
+            }
         }
-        else {
-            KIWI_LOG(CRITICAL, "Failed to open project by the path: {}. Reason: {}",
-                m_activeProjectPath.string(),
-                project.GetError().GetDescription()
+
+
+        Result<std::vector<nlohmann::json>> allProjectsRetrieveRes = m_projectsDB->Execute("SELECT * FROM Projects");
+        if (!allProjectsRetrieveRes) {
+            KIWI_LOG(ERROR, "Failed to retrieve all projects from the database: {}",
+                allProjectsRetrieveRes.GetError().GetDescription()
             );
             return false;
         }
+
+        for (const nlohmann::json& project : *allProjectsRetrieveRes) {
+            ProjectDescription projectDesc;
+            projectDesc.projectID = project.value("Id", std::numeric_limits<u64>::max());
+            projectDesc.projectName = project.value("ProjectName", std::string());
+            projectDesc.path = project.value("Path", std::filesystem::path());
+
+            if (!Project::Validate(projectDesc.path)) {
+                projectDesc.isCorrupted = true;
+            }
+
+            m_allProjectsList.push_back(projectDesc);
+        }
+
+        return true;
     }
 
     std::shared_ptr<Project> ProjectSubsystem::GetActiveProject() const {
         KIWI_ASSERT_BASIC(m_activeProject);
 
         return m_activeProject;
+    }
+
+    const std::vector<ProjectSubsystem::ProjectDescription>& ProjectSubsystem::GetAllProjects() const {
+        return m_allProjectsList;
+    }
+
+
+    bool ProjectSubsystem::LoadProjectsDatabase() {
+        constexpr StringView databaseCreateQuery = KIWI_MAKE_STRING(
+            CREATE TABLE IF NOT EXISTS Projects(
+                Id INTEGER PRIMARY KEY NOT NULL,
+                ProjectName TEXT NOT NULL,
+                Path TEXT NOT NULL,
+                PreviewImage BLOB
+            );
+        );
+
+
+        m_projectsDB = std::make_unique<SQLiteDatabase>();
+        if (Result<void> res = m_projectsDB->Open(GetProjectsDatabasePath())) {
+            const Result<std::vector<nlohmann::json>> createRes = m_projectsDB->Execute(databaseCreateQuery);
+            if (createRes) {
+                return true;
+            }
+
+            KIWI_CTX_LOG(ERROR, "Failed to create projects database file: {}",
+                createRes.GetError().GetDescription()
+            );
+            return false;
+        }
+        else {
+            KIWI_CTX_LOG(ERROR, "Failed to open projects database file: {}",
+                res.GetError().GetDescription()
+            );
+            return false;
+        }
+    }
+
+    bool ProjectSubsystem::LoadActiveProject() {
+        KIWI_ASSERT_BASIC(m_projectsDB->IsOpen());
+        KIWI_ASSERT_BASIC(m_activeProjectID.HasValue() && *m_activeProjectID >= 0)
+
+        const String pathRetrieveQuery = String::Format(
+            "SELECT Path FROM Projects WHERE Id={};",
+            *m_activeProjectID
+        );
+
+        if (Result<std::vector<nlohmann::json>> res = m_projectsDB->Execute(pathRetrieveQuery.ToStringView())) {
+            const std::filesystem::path path = res->at(0)["Path"];
+
+            if (path.empty()) {
+                KIWI_LOG(ERROR, "An active project path successfully retrieved, but the path itself is empty");
+                return false;
+            }
+
+            if (Result<std::shared_ptr<Project>> proj = Project::Open(path)) {
+                m_activeProject = *proj;
+                return true;
+            }
+            else {
+                KIWI_LOG(ERROR, "Failed to open exising project: {}", proj.GetError().GetDescription());
+                return false;
+            }
+        }
+        else {
+            KIWI_LOG(ERROR, "Failed to retrieve an active project path: {}",
+                res.GetError().GetDescription()
+            );
+
+            return false;
+        }
     }
 }
--- engine/core/ProjectSubsystem.hpp
+++ engine/core/ProjectSubsystem.hpp
@@ -2,23 +2,49 @@
 
 #include <core/Object.hpp>
 
+#include <nlohmann/json_fwd.hpp>
+
 
 namespace Kiwi {
     class Project;
+    KIWI_INTERFACE ISqlDatabase;
 
 
     class ProjectSubsystem final : public ASubsystem {
         KIWI_CREATE_OBJECT(ProjectSubsystem, ASubsystem)
 
+        static constexpr StringView PROJECTS_DB_FILE_NAME = "Projects.db";
+
     public:
-        explicit ProjectSubsystem(std::filesystem::path projectPath);
+        struct ProjectDescription {
+            u64 projectID = 0;
+            String projectName;
+            std::filesystem::path path;
+            // TODO: add preview image
+
+            bool isCorrupted = false;
+        };
+
+        KIWI_NODISCARD static std::filesystem::path GetProjectsDatabasePath();
+
+    public:
+        explicit ProjectSubsystem(Opt<i32> activeProjectID);
 
         KIWI_NODISCARD bool Init() override;
         KIWI_NODISCARD std::shared_ptr<Project> GetActiveProject() const;
 
-    private:
-        std::filesystem::path m_activeProjectPath;
+        KIWI_NODISCARD const std::vector<ProjectDescription>& GetAllProjects() const;
 
+    private:
+        KIWI_NODISCARD bool LoadProjectsDatabase();
+        KIWI_NODISCARD bool LoadActiveProject();
+
+    private:
+        Opt<i32> m_activeProjectID;
+
+        std::vector<ProjectDescription> m_allProjectsList;
+
+        std::unique_ptr<ISqlDatabase> m_projectsDB;
         std::shared_ptr<Project> m_activeProject;
     };
 }
