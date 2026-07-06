81be82800fea46d5d55cd1b1ded1232e0aaa12ce
qulop <qlopsimail@gmail.com>
2026-07-06 18:17:55 +0300

    feat(driver/opengl): the `GraphicObjectsFactoryGL` updated to according with a new `IGraphicObjectsFactory` interface

--- engine/driver/opengl/GraphicObjectsFactoryGL.cpp
+++ engine/driver/opengl/GraphicObjectsFactoryGL.cpp
@@ -6,6 +6,14 @@
 
 
 namespace Kiwi::OpenGL {
+    std::shared_ptr<IRenderContext> GraphicObjectsFactoryGL::CreateContext() const {
+        return std::make_shared<RenderContextGL>();
+    }
+
+    std::shared_ptr<AGraphicDevice> GraphicObjectsFactoryGL::CreateGraphicDevice() const {
+        return nullptr;
+    }
+
     std::shared_ptr<AShaderCompiler> GraphicObjectsFactoryGL::CreateShaderCompiler() const {
         return std::make_shared<ShaderCompilerGL>();
     }
--- engine/driver/opengl/GraphicObjectsFactoryGL.hpp
+++ engine/driver/opengl/GraphicObjectsFactoryGL.hpp
@@ -6,8 +6,9 @@
 namespace Kiwi::OpenGL {
     class GraphicObjectsFactoryGL : public IGraphicObjectsFactory {
     public:
-        KIWI_NODISCARD std::shared_ptr<ARenderPipeline> CreateRenderPipeline() const override;
-
+        KIWI_NODISCARD std::shared_ptr<IRenderContext> CreateContext() const override;
+        
+        KIWI_NODISCARD std::shared_ptr<AGraphicDevice> CreateGraphicDevice() const override;
         KIWI_NODISCARD std::shared_ptr<AShaderCompiler> CreateShaderCompiler() const override;
 
         ~GraphicObjectsFactoryGL() override = default;
