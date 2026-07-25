3e4cbf31814e724ff90b8f77d2748abf6a0eedb5
qulop <qlopsimail@gmail.com>
2026-07-25 10:45:04 +0300

    feat(driver/opengl): started texture resource creation

--- engine/driver/opengl/TextureGL.cpp
+++ engine/driver/opengl/TextureGL.cpp
@@ -2,14 +2,31 @@
 
 
 namespace Kiwi::OpenGL {
-    Texture2DGL::Texture2DGL(const ImageDesc& imageDesc)
-    {}
+    Texture2DGL* Texture2DGL::Create(const ImageDesc& desc) {
+        Texture2DGL* newTexture = new Texture2DGL();
+
+        GlID& id = newTexture->m_id;
+
+        glGenTextures(1, &id);
+        glBindTexture(GL_TEXTURE_2D, id);
+
+        // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, )
+
+        // glTexImage2D(GL_TEXTURE_2D, 0, )
+
+        glBindTexture(GL_TEXTURE_2D, 0);
+
+        newTexture->m_width = desc.width;
+        newTexture->m_height = desc.height;
+
+        return newTexture;
+    }
 
     u32 Texture2DGL::GetWidth() const {
-
+        return m_width;
     }
 
     u32 Texture2DGL::GetHeight() const {
-
+        return m_height;
     }
 }
--- engine/driver/opengl/TextureGL.hpp
+++ engine/driver/opengl/TextureGL.hpp
@@ -2,18 +2,25 @@
 
 #include <renderer/Texture.hpp>
 
+#include <driver/opengl/core/TypesGL.hpp>
+
 
 namespace Kiwi::OpenGL {
     class Texture2DGL : public ATexture2D {
         KIWI_CREATE_OBJECT(Texture2DGL, ATexture2D)
 
     public:
-        explicit Texture2DGL(const ImageDesc& imageDesc);
+        KIWI_NODISCARD static Texture2DGL* Create(const ImageDesc& desc);
 
-        u32 GetWidth() const override;
-        u32 GetHeight() const override;
+        KIWI_NODISCARD u32 GetWidth() const override;
+        KIWI_NODISCARD u32 GetHeight() const override;
 
         ~Texture2DGL() override = default;
+
+    private:
+        GlID m_id = KIWI_GL_UNDEFINED_ID;
+        u32 m_width = 0;
+        u32 m_height = 0;
     };
 
 }
