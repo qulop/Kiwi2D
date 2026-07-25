0d1e854d2e43fbe0963546110cdd26cdd9b3a135
qulop <qlopsimail@gmail.com>
2026-07-25 10:13:19 +0300

    feat(core): added generational resource handles

--- engine/core/Handle.hpp
+++ engine/core/Handle.hpp
@@ -1,22 +1,108 @@
 #pragma once
 
 #include <common/Definitions.hpp>
+#include <common/meta/Concepts.hpp>
 
 
 #define KIWI_INVALID_HANDLE     UINT32_MAX
 
-#define KIWI_HANDLE(HandleName)                         \
-    struct HandleName {                                 \
-        u32 handle = 0;                                 \
-                                                        \
-        KIWI_NODISCARD constexpr bool IsValid() const { \
-            return handle != KIWI_INVALID_HANDLE;       \
-        }                                               \
-    };
+#define KIWI_HANDLE(HandleName, UnderlyingHandleType)       \
+    struct HandleName : HandleBase {                        \
+        using UnderlyingType = UnderlyingHandleType;        \
+        using UnderlyingTypePtr = UnderlyingHandleType*;    \
+    }
+
 
 namespace Kiwi {
-    KIWI_HANDLE(IndexBufferHandle)
-    KIWI_HANDLE(VertexBufferHandle)
-    KIWI_HANDLE(TextureHandle)
-    KIWI_HANDLE(ShaderHandle)
+    class AIndexBuffer;
+    class AVertexBuffer;
+    class ATexture;
+    class AShader;
+
+
+    struct HandleBase {
+        u32 handle = KIWI_INVALID_HANDLE;
+        u32 generation = 0;
+
+        KIWI_NODISCARD constexpr bool IsValid() const {
+            return handle != KIWI_INVALID_HANDLE;
+        }
+    };
+
+
+
+    template<Concepts::DerivedFrom<HandleBase> THandle>
+    class HandlePool {
+    public:
+        using HandleType = THandle;
+        using HandleObjectPtr = THandle::UnderlyingTypePtr;
+
+        struct Slot {
+            HandleObjectPtr object = nullptr;
+            u32 generation = 1;
+        };
+
+    public:
+        KIWI_NODISCARD HandleObjectPtr Get(HandleType handle) {
+            if (!IsValidHandle(handle)) {
+                return nullptr;
+            }
+
+            return m_pool.at(handle.handle).object;
+        }
+
+        KIWI_NODISCARD bool IsValidHandle(HandleType handle) const {
+            return handle.IsValid()
+                && handle.handle < m_pool.size()
+                && m_pool.at(handle.handle).object != nullptr
+                && handle.generation == m_pool.at(handle.handle).generation;
+        }
+
+        KIWI_NODISCARD HandleType Add(HandleObjectPtr obj) {
+            HandleType newHandle;
+
+            
+            if (!m_freeSlots.empty()) {
+                newHandle.handle = *m_freeSlots.begin();
+                m_freeSlots.erase(newHandle.handle);
+            }
+            else {
+
+            }
+
+            Slot& slot = m_pool.at(newHandle.handle);
+            slot.object = obj;
+
+            newHandle.generation = slot.generation;
+
+            return newHandle;
+        }
+
+
+        KIWI_NODISCARD bool Remove(HandleType handle) {
+            if (!IsValidHandle(handle)) {
+                return false;
+            }
+
+            Slot& slot = m_pool.at(handle.handle);
+
+            delete slot.object;
+            slot.object = nullptr;
+
+            ++slot.generation;
+
+            m_freeSlots.insert(handle.handle);
+            return true;
+        }
+
+
+    private:
+        std::vector<Slot> m_pool;
+        std::set<u32> m_freeSlots;
+    };
+
+    KIWI_HANDLE(IndexBufferHandle, AIndexBuffer);
+    KIWI_HANDLE(VertexBufferHandle, AVertexBuffer);
+    KIWI_HANDLE(TextureHandle, ATexture);
+    KIWI_HANDLE(ShaderHandle, AShader);
 }
