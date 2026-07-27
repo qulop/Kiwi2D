75e5b76294e508731d5007b4213898eddea36dde
qulop <qlopsimail@gmail.com>
2026-07-27 18:11:42 +0300

    feat(math): added a class-wrapper around float, which is representing an angle in radians

---
+++ engine/math/Angle.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <common/Definitions.hpp>
+
+#include <glm/glm.hpp>
+
+
+namespace Kiwi {
+    struct Radians {
+    public:
+        KIWI_NODISCARD static constexpr Radians FromDegree(f32 angle) {
+            return Radians(glm::radians(angle));
+        }
+
+    public:
+        f32 value = 0.f;
+
+        Radians() = default;
+        explicit Radians(f32 rad) :
+            value(rad)
+        {}
+
+        KIWI_NODISCARD constexpr f32 ToDegrees() const {
+            return glm::degrees(value);
+        }
+
+        KIWI_NODISCARD constexpr f32 operator*() const noexcept {
+            return value;
+        }
+
+        KIWI_NODISCARD constexpr operator float() const noexcept {
+            return value;
+        }
+    };
+}
