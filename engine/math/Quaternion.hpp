aa9e8c1659ecb3c9090222cc31bdd16e43ec07ab
qulop <qlopsimail@gmail.com>
2026-07-29 02:43:13 +0300

    feat(math): added `GLM_ENABLE_EXPERIMENTAL` macro definition to fix the CE

--- engine/math/Quaternion.hpp
+++ engine/math/Quaternion.hpp
@@ -8,8 +8,11 @@
 
 #include <glm/glm.hpp>
 #include <glm/gtc/quaternion.hpp>
-#include <glm/gtx/quaternion.hpp>
 #include <glm/detail/type_quat.hpp>
+
+#define GLM_ENABLE_EXPERIMENTAL
+    #include <glm/gtx/quaternion.hpp>
+#undef GLM_ENABLE_EXPERIMENTAL
 
 
 
